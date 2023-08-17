cab96fe5326c39ebb5e294e6c9be766db40f14a1
Luiz Franco <luizfranco@wine.com.br>
2023-08-17 13:08:41 -0300

    feat: remove logo in footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,5 +1,4 @@
 import * as S from './styles'
-import logo from '../../img/logomestre.svg'
 import phrase from '../../img/purshe.png'
 import screenone from '../../img/screenone.png'
 import screentwo from '../../img/screentwo.png'
